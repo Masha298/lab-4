76611a4c6c275bba960ce2d0c5b11f83822e1ec5
Maria <133533183+Masha298@users.noreply.github.com>
2024-05-06 18:45:43 +0500

    Add files via upload

---
+++ UserEntity.java
@@ -0,0 +1,85 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @Column(name = "id")
+    private int idUser;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "pass")
+    private String password;
+
+    @Column(name = "mail")
+    private String email;
+
+    @Id
+    @Column(name = "id")
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "pass")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "mail")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (idUser != that.idUser) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        if (!Objects.equals(password, that.password)) return false;
+        if (!Objects.equals(email, that.email)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idUser;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
